5c9214c79ad96d6314b59c9f2b7596db8d3b5c88
valentinbureau <valentin-bureau2@outlook.fr>
2021-03-08 17:30:07 +0100

    Update of master - Boundaries of window

--- zelda/core/src/com/zelda/JoueurInter.java
+++ zelda/core/src/com/zelda/JoueurInter.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.zelda.world.GameMap;
 
 //Les méthodes sont après les getters et setters
 
@@ -13,12 +14,24 @@ public class JoueurInter {
 	private float linkX;
 	private float linkY;
 	private float linkSpeed;
+	private float defaultSpeed;
+	GameMap map= new GameMap();
 	
+	private boolean canUp;
+	private boolean canDown;
+	private boolean canLeft;
+	private boolean canRight;
 	public JoueurInter(float linkX, float linkY, float linkSpeed) {
 		super();
 		this.linkX = linkX;
 		this.linkY = linkY;
 		this.linkSpeed = linkSpeed;
+		this.defaultSpeed = linkSpeed;
+		this.canUp=true;
+		this.canDown=true;
+		this.canLeft=true;
+		this.canRight=true;
+		
 	}
 
 	public JoueurInter() {
@@ -66,18 +79,40 @@ public class JoueurInter {
 	}
 
 	public void create () {
-		this.sprite= new Sprite( new Texture ("link.png"));
+		this.linkTexture = new Texture ("link.png");
+		this.sprite= new Sprite( linkTexture );
 	}
 	
 	public void render() {
-		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) 
-			   linkX -= Gdx.graphics.getDeltaTime() * linkSpeed;
-		if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) 
-				linkX += Gdx.graphics.getDeltaTime() * linkSpeed;
-		if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) 
-				linkY += Gdx.graphics.getDeltaTime() * linkSpeed;
-		if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) 
-				linkY -= Gdx.graphics.getDeltaTime() * linkSpeed;
+		
+		if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) {
+			linkSpeed = defaultSpeed;
+			if(linkX < 0 || !canLeft) {
+				linkSpeed=0;
+			}
+			else {canLeft=true;}
+			linkX -= Gdx.graphics.getDeltaTime() * linkSpeed;}
+		if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) {
+			linkSpeed = defaultSpeed;
+			if(linkX > map.getWidth()-this.linkTexture.getWidth() || !canRight) {
+				linkSpeed=0;
+			}
+			else {canRight=true;}
+				linkX += Gdx.graphics.getDeltaTime() * linkSpeed;}
+		if(Gdx.input.isKeyPressed(Keys.DPAD_UP)) {
+			linkSpeed = defaultSpeed;
+			if(linkY >  map.getHeight()-this.linkTexture.getHeight() || !canUp) {
+				linkSpeed=0;
+			}
+			else {canUp=true;}
+				linkY += Gdx.graphics.getDeltaTime() * linkSpeed;}
+		if(Gdx.input.isKeyPressed(Keys.DPAD_DOWN)) {
+			linkSpeed = defaultSpeed;
+			if(linkY < 0 || !canDown) {
+				linkSpeed=0;
+			}
+			else {canDown=true;}
+				linkY -= Gdx.graphics.getDeltaTime() * linkSpeed;}
 	}
 	
 }
